69bf74acf6a1ead0bfbd25c594382759105b5105
DESKTOP-RDRAI9D\LEAPSY <s8871404@gmail.com>
2016-11-25 16:20:17 +0800

    gitignore

--- openCVLibrary300/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary300/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/chenweilun/Documents/Android-code/OpenCVDemo/OpenCVDemo/openCVLibrary300/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: D:\\AndroidCode\\OpenCVDemo\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
