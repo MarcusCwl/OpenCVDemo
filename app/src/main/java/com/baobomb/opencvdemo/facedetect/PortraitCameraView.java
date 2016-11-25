7162aa9e74d6fb1cb716de2d8441de4c0bc88c4f
DESKTOP-RDRAI9D\LEAPSY <s8871404@gmail.com>
2016-11-25 17:22:22 +0800

    Add take picture function

--- app/src/main/java/com/baobomb/opencvdemo/facedetect/PortraitCameraView.java
+++ app/src/main/java/com/baobomb/opencvdemo/facedetect/PortraitCameraView.java
@@ -18,9 +18,10 @@ import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.FileOutputStream;
 import java.util.List;
 
-public class PortraitCameraView extends CameraBridgeViewBase implements PreviewCallback {
+public class PortraitCameraView extends CameraBridgeViewBase implements PreviewCallback,Camera.PictureCallback {
 
     private static final int MAGIC_TEXTURE_ID = 10;
     private static final String TAG = "JavaCameraView";
@@ -35,6 +36,8 @@ public class PortraitCameraView extends CameraBridgeViewBase implements PreviewC
     protected JavaCameraFrame[] mCameraFrame;
     private SurfaceTexture mSurfaceTexture;
     private int mCameraId;
+    private String mPictureFileName;
+
 
     public static class JavaCameraSizeAccessor implements ListItemAccessor {
 
@@ -47,6 +50,37 @@ public class PortraitCameraView extends CameraBridgeViewBase implements PreviewC
             Camera.Size size = (Camera.Size) obj;
             return size.height;
         }
+    }
+
+    public void takePicture(final String fileName) {
+        Log.i(TAG, "Taking picture");
+        this.mPictureFileName = fileName;
+        // Postview and jpeg are sent in the same buffers if the queue is not empty when performing a capture.
+        // Clear up buffers to avoid mCamera.takePicture to be stuck because of a memory issue
+        mCamera.setPreviewCallback(null);
+
+        // PictureCallback is implemented by the current class
+        mCamera.takePicture(null, null, this);
+    }
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        Log.i(TAG, "Saving a bitmap to file");
+        // The camera preview was automatically stopped. Start it again.
+        mCamera.startPreview();
+        mCamera.setPreviewCallback(this);
+
+        // Write the image in a file (in jpeg format)
+        try {
+            FileOutputStream fos = new FileOutputStream(mPictureFileName);
+
+            fos.write(data);
+            fos.close();
+
+        } catch (java.io.IOException e) {
+            Log.e("PictureDemo", "Exception in photoCallback", e);
+        }
+
     }
 
     public PortraitCameraView(Context context, int cameraId) {
@@ -236,14 +270,13 @@ public class PortraitCameraView extends CameraBridgeViewBase implements PreviewC
             mCamera.addCallbackBuffer(mBuffer);
     }
 
-    private class JavaCameraFrame implements CvCameraViewFrame {
+    private class JavaCameraFrame implements CvCameraViewFrame{
         private Mat mYuvFrameData;
         private Mat mRgba;
         private int mWidth;
         private int mHeight;
         private Mat mRotated;
         private Mat mRotatedGray;
-
 
         public Mat gray() {
             if (mRotatedGray != null) mRotatedGray.release();
